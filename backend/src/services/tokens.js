9cca54db2e05fb5db6b355d925f651753f1cf7e4
Fernando <fguatimosim@gmail.com>
2023-03-01 16:53:23 -0300

    implemented jwt verification and tests

--- backend/src/services/tokens.js
+++ backend/src/services/tokens.js
@@ -2,11 +2,21 @@ require("dotenv-safe").config()
 const jwt = require('jsonwebtoken');
 
 function generateJWT(payload) {
-    const token = jwt.sign(payload, process.env.SECRET, {
+    const token = jwt.sign(payload, process.env.JWT_KEY, {
         expiresIn:'5m'
     })
 
     return token
 }
 
-module.exports = { generateJWT }
+function validateJWT(token) {
+    try {
+        const payload = jwt.verify(token, process.env.JWT_KEY)
+        return payload
+        
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = { generateJWT, validateJWT }
--- backend/test/services/tokens.test.js
+++ backend/test/services/tokens.test.js
@@ -1,6 +1,9 @@
-const { generateJWT } = require('../../src/services/tokens.js')
+const { generateJWT, validateJWT } = require('../../src/services/tokens.js')
+// const { useFakeTimers, advanceTimersByTime } = require('jest')
 
 describe('Test the functions responsible for dealing with standard jwts', () => {
+    jest.useFakeTimers();
+    
     test('generateJWT must return a string', () => {
         const testPayload = {
             id: '1',
@@ -9,5 +12,32 @@ describe('Test the functions responsible for dealing with standard jwts', () => 
 
         const token = generateJWT(testPayload)
         expect(token).toEqual(expect.any(String))
+    })
+
+    test('validateJWT must return the entered payload', () => {
+        const testPayload = {
+            id: '1',
+            name: 'teste'
+        }
+
+        const token = generateJWT(testPayload)
+        const result = validateJWT(token)
+        expect(result).toEqual(expect.objectContaining(testPayload))
+    })
+
+    test('validateJWT must throw error if token is expired', () => {
+        const testPayload = {
+            id: '1',
+            name: 'teste'
+        }
+
+        const token = generateJWT(testPayload)
+        jest.advanceTimersByTime(5 * 60 * 1000 + 1)
+        
+        function testFunction() {
+            const result = validateJWT(token)
+            return result
+        }
+        expect(testFunction).toThrow(Error)
     })
 })
