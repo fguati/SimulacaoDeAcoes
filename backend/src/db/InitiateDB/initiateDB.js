5d11c3b6a1d70f8b1159a8a9716a745627efee43
Fernando <fguatimosim@gmail.com>
2023-04-13 17:20:31 -0300

    docs: :memo: Commented the initiateDB script

--- backend/src/db/InitiateDB/initiateDB.js
+++ backend/src/db/InitiateDB/initiateDB.js
@@ -1,11 +1,11 @@
-const fs = require('fs')
 const sqlite3 = require('sqlite3').verbose()
-
 const SCHEMA_LIST = require('../Schemas')
 const createDB = require('../utils/ManipulateDBFiles/createDB')
 
+//Create the file for the sqlite database, in case it doesn't already exists, and store its filepath in a constant for later use
 const dbFilePath = createDB('db')
 
+//Create instance of connection with the database
 const db = new sqlite3.Database(dbFilePath, (err) => {
     if(err){
         return console.log(`Erro na abertura do db: ${err.message}`)
@@ -13,6 +13,7 @@ const db = new sqlite3.Database(dbFilePath, (err) => {
     console.log('Connected to db')
 })
 
+//Run sql to create any table still not in the database.
 db.serialize(() => {
     // db.run('PRAGMA foreign_keys=ON',(err) => {
     //     return console.log(`Erro no FK do db: ${err.message}`)
@@ -27,6 +28,7 @@ db.serialize(() => {
 
 })
 
+//Logs the exit of the db
 process.on('SIGINT',() => {
     return db.close((err) => {
         if(err){
