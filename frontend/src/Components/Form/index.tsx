abfa60ee1ac7691350f212662ac87a336349331d
Fernando <fguatimosim@gmail.com>
2023-04-14 13:33:17 -0300

    docs: :memo: Documented Form component

--- frontend/src/Components/Form/index.tsx
+++ frontend/src/Components/Form/index.tsx
@@ -4,9 +4,18 @@ import FormContainer from "./FormContainer"
 import IFormProps from "./IFormProps"
 import useFormFields from "./useFormFields"
 
+/**component for forms. Receives a list of fields, rendered them all as input 
+ * fields with the inputs and their labels, and render a submit button as well 
+ * that will call the onSubmit functio received as props
+*/
 function Form({fields, onSubmit}:IFormProps) {
+    //create a state for the field values of each input field
     const { currentFieldValues, setFieldValue, clearForm } = useFormFields(fields)
 
+    /**wrapper function that gives the basic standard behavior of a form
+     * submission. It guarantees that the submission won't reload the
+     * page and that after the onSubmit function is called the form is cleared
+    */
     const baseOnSubmit = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         onSubmit(e)
