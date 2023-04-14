8f537406097d18d603b0176909ffdafc4661d5a2
Fernando <fguatimosim@gmail.com>
2023-04-14 13:32:27 -0300

    docs: :memo: Documented frontend contexts

--- frontend/src/Common/Contexts/SessionContext.tsx
+++ frontend/src/Common/Contexts/SessionContext.tsx
@@ -11,14 +11,32 @@ interface ISessionContext {
     getLogInStatus?: () => boolean
 }
 
+/**Session context will keep data relating to the session, 
+ * receiving it initially from local storage sources 
+*/
 const SessionContext = createContext<ISessionContext>({})
 
-
+/**
+ * Session Provider will provide session data, initially
+ * received from local storage, cookies, cache, etc.
+ */
 const SessionProvider = ({ children }: Props) => {
+    /**create a state of logged in that determines 
+     * if user is already logged in. Initial value
+     * is determined by whether or not user has an
+     * authToken cookie
+    */
     let authCookie = getCookie('authToken')
     const currentlyLoged = authCookie !== ''
     const [loggedIn, setLogIn] = useState(currentlyLoged)
 
+    /**
+     * the log in status must be accessed only through
+     * this function, because it first checks the cookies
+     * for authTokens, making sure that, if the status of
+     * the cookie changed, the loged in status will have 
+     * changed as well
+     */
     function getLogInStatus() {
         authCookie = getCookie('authToken')
         if(!authCookie){
