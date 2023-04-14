ae5be7bfbc5756909cfcb1b7e91ecdbcbe5c9898
Fernando <fguatimosim@gmail.com>
2023-04-14 12:49:27 -0300

    docs: :memo: Documented backend utils

--- backend/src/utils/globalVariables.js
+++ backend/src/utils/globalVariables.js
@@ -1,5 +1,12 @@
+//global variables used in multiple modules of the app 
+
+//list of the data that a user needs to have
 const userPropertyList = ['username', 'email', 'hashed_password']
+
+//number of seconds for a token to expire
 const authTokenDurationInSec = 30 * 60
+
+//path of the directory where the db file will be
 const dbFileDir = 'C:\\Users\\nando\\Desktop\\Projetos Programação\\Projetos Pessoais\\Simulação Ações\\SimulacaoDeAcoes\\backend\\src\\db'
 
 module.exports = { userPropertyList, authTokenDurationInSec, dbFileDir }
--- backend/src/utils/invalidInputFunctions.js
+++ backend/src/utils/invalidInputFunctions.js
@@ -1,7 +1,17 @@
+/**receives a list of values that will be entered as arguments in 
+ * a function or method and checks if any of them in invalid.
+ * Used to validate data received through forms
+*/
 function hasInvalidParam(paramList) {
     return paramList.some(param => !param)
 }
 
+
+/**
+ * Receives an object and a list of properties this object should
+ * have. Makes then a list of every property of the list that does 
+ * not have a valid value in the object
+ */
 function listInvalidInputs(objectWithInputs, listOfInputNames) {
     let resultList = listOfInputNames.map((inputName) => {
         const inputValue = objectWithInputs[inputName]
