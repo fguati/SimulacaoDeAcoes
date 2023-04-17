59be81e43a91f1b3a31798bd3da430a9ae598971
Fernando <fguatimosim@gmail.com>
2023-04-17 10:07:43 -0300

    docs: :memo: Documented files of the sign up page

--- frontend/src/Pages/SignUp/index.tsx
+++ frontend/src/Pages/SignUp/index.tsx
@@ -3,10 +3,11 @@ import PageLayout from "Components/PageLayout";
 import IFormField from "Interfaces/IFormField";
 import useSubmitSignUp from "./utils/useSubmitSignUp";
 
-
 function SignUpPage() {
+    //Hook that returns function responsible for handling the submission of the register form
     const submitSignUp = useSubmitSignUp()
     
+    //input fields present in the register user form
     const fields:IFormField[] = [
         {name: 'Username', type: 'text', value:''},
         {name: 'E-mail', type:'email', value:''}, 
--- frontend/src/Pages/SignUp/utils/useSignUpSuccessHandler.ts
+++ frontend/src/Pages/SignUp/utils/useSignUpSuccessHandler.ts
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-
+/**
+ * Function that handles success responses from http requests
+ * to the backend API
+ */
 const useSignUpSuccessHandler = () => {
     const navigate = useNavigate()
     async function signUpSuccessHandler(response:Response) {
