e192d1b79e6e34589ff5f7eb2fec3c1c69d9f17a
Fernando <fguatimosim@gmail.com>
2023-04-17 10:46:08 -0300

    Finished documentation

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -4,6 +4,7 @@ import { CookiesProvider } from 'react-cookie';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import routes from 'routes';
 
+//App router rendered by react. It receives the routes implemented in the routes folder and nests them inside Global Providers 
 function App() {
   const router = createBrowserRouter(routes)
 
