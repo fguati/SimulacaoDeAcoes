15f91ceeeaa4771d2f8bc497031b09c983a5756b
Fernando <fguatimosim@gmail.com>
2023-04-19 17:22:43 -0300

    test: :white_check_mark: Refactored, commented and tested Snackbar container

--- frontend/src/Components/Snackbar/SnackbarContainer/index.tsx
+++ frontend/src/Components/Snackbar/SnackbarContainer/index.tsx
@@ -1,26 +1,49 @@
 import styled from "styled-components";
 import IProps from "./IProps";
-import snackThemeSelector from "./snackThemeSelector";
+import { transitionTime } from "Common/Constants";
+import { BoxColorPalette } from "Common/Types/ColorPalletes";
+import BoxColorThemes from "Common/BoxColorThemes";
 
+//function to receive snackbar position from props
+const snackPosition = ({ position }:IProps) => {
+    return `translateY(${position})`
+}
+
+// function to receive snackbar colorPallete from props and return the color of the selected property
+const snackThemeSelector = (colorPalette: BoxColorPalette, property: 'background' | 'line') => {
+    return `var(${BoxColorThemes[colorPalette][property]})`
+}
+
+//Container that handle visual elements of the snackbar
 const SnackbarContainer = styled.div`
+    //position style
     z-index: 2;
     position: absolute;
-    transform: translateY(-6vh);
+    transform: ${snackPosition};
     
+    //spacing style
     padding: var(--default-spacing);
-    background-color: ${({colorPallete}:IProps) => snackThemeSelector({colorPallete}, 'background')};
+    
+    //box style
+    background-color: ${({colorPalette}:IProps) => snackThemeSelector(colorPalette, 'background')};
     box-sizing: border-box;
     width: 100%;
     height: 4vh;
     
-    border: 2px solid ${({colorPallete}:IProps) => snackThemeSelector({colorPallete}, 'line')};
+    //border style
+    border: 2px solid ${({colorPalette}:IProps) => snackThemeSelector(colorPalette, 'line')};
     border-radius: 5px;
 
+    //text style
     text-align: center;
-    color: ${({colorPallete}:IProps) => snackThemeSelector({colorPallete}, 'line')};
+    color: ${({colorPalette}:IProps) => snackThemeSelector(colorPalette, 'line')};
     display: flex;
     align-items: center;
     justify-content: center;
+
+    //interactive style
+    cursor: pointer;
+    transition: ${transitionTime}ms;
     
 `
 
---
+++ frontend/src/Components/Snackbar/tests/SnackbarContainer.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react"
+import SnackbarContainer from "../SnackbarContainer"
+import 'styles/_variables.css'
+import '@testing-library/jest-dom'
+import ISnackPosition from "Common/Types/ISnackPosition"
+
+describe('Test the rendering of the snackbar container', () => {
+    test('position on the screen must be defined by props', () => {
+        const testPosition: ISnackPosition = '-5vh'
+        
+        render(<SnackbarContainer
+            colorPallete="neutral"
+            position={testPosition}
+        >test</SnackbarContainer>)
+        
+        const $container = screen.getByText('test')
+        const style = window.getComputedStyle($container)
+        expect(style.transform).toBe(`translateY(${testPosition})`)
+    })
+})
