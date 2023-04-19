8e1c46ddfd60f17c17b9f9a4fad55c8e9c23ddd0
Fernando <fguatimosim@gmail.com>
2023-04-19 16:46:14 -0300

    Commented Snackbar context

--- frontend/src/Common/Contexts/SnackbarContext/index.tsx
+++ frontend/src/Common/Contexts/SnackbarContext/index.tsx
@@ -2,44 +2,59 @@ import ReactChildren from 'Common/Types/ReactChildren'
 import { createContext, useState } from 'react'
 import ISnackPosition, { BottomOfScreenPos, OutOfScreenPos } from './ISnackPosition'
 import { transitionTime } from 'Common/Constants'
-import { ColorPallete } from 'Components/Snackbar/SnackbarContainer/IProps'
+import { ColorPalette } from 'Components/Snackbar/SnackbarContainer/IProps'
 
+//Props interface for the context provider
 interface IProps {
     children: ReactChildren
 }
 
-interface ISnack {
-    active?: boolean
-    deactivateSnackbar?: () => void
-    activateSnackbar?: (pallete?: ColorPallete) => void
-    snackBarPosition?: ISnackPosition
-    colorPallete?: ColorPallete
+//Interface for the context to be created
+export interface ISnackbarContext {
+    active: boolean
+    deactivateSnackbar: () => void
+    activateSnackbar: (pallete?: ColorPalette) => void
+    snackBarPosition: ISnackPosition
+    colorPalette: ColorPalette
 }
 
-const SnackbarContext = createContext<ISnack>({})
+//Create context that will be responsible for handling the snackbar so be rendered and manipulated from anywhere in the app
+const SnackbarContext = createContext<ISnackbarContext>(undefined!)
 
+//Context provider for the snackbar context
 const SnackbarProvider = ({ children }: IProps) => {
+    //state determining whether snackbar will be rendered or not
     const [active, setActive] = useState(false)
+    //state determining where the snackbar will be in the screen
     const [snackBarPosition, setSnackbarPosition] = useState<ISnackPosition>(OutOfScreenPos)
-    const [colorPallete, setColorPallete] = useState<ColorPallete>('neutral')
+    //state determining the color palette
+    const [colorPalette, setColorPalette] = useState<ColorPalette>('neutral')
 
+    // Function responsible for unmounting the snackbar
     function deactivateSnackbar() {
+        // position the snackbar out of the screen for a better visual effect
         setSnackbarPosition(OutOfScreenPos)
+        
+        //unmount the snackbar, waiting enough time for the transition animation to complete
         setTimeout(() => {
             setActive(false)
         }, transitionTime)
     }
 
-    function activateSnackbar(desiredPallete: ColorPallete = 'neutral') {
+    //Function responsible for rendering snackbar
+    function activateSnackbar(desiredPalette: ColorPalette = 'neutral') {
+        //Render the snackbar
         setActive(true)
-        setColorPallete(desiredPallete)
+        //Change the snackbar to the color palette entered as argument
+        setColorPalette(desiredPalette)
+        // Move the snackbar into view, giving it enough time for the transition animation to complete
         setTimeout(() => {
             setSnackbarPosition(BottomOfScreenPos)
         }, transitionTime / 5)
     }
 
     return (
-        <SnackbarContext.Provider value={{ active, deactivateSnackbar, activateSnackbar, snackBarPosition, colorPallete }}>
+        <SnackbarContext.Provider value={{ active, deactivateSnackbar, activateSnackbar, snackBarPosition, colorPalette }}>
             {children}
         </SnackbarContext.Provider>
     )
