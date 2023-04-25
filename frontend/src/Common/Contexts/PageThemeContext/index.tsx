8b72258707fa7d61709025245985af43353de507
Fernando <fguatimosim@gmail.com>
2023-04-25 16:52:20 -0300

    feat: :sparkles: Created dark mode toggling functionality

--- frontend/src/Common/Contexts/PageThemeContext/index.tsx
+++ frontend/src/Common/Contexts/PageThemeContext/index.tsx
@@ -1,20 +1,23 @@
 import { ReactChildren } from "Common/Types";
 import { createContext, useState } from "react";
 
+//Interface of values and methods provided by the page theme context
 interface IPageContextValues {
     pageTheme: string
     setPageTheme: React.Dispatch<React.SetStateAction<string>>
     toggleDarkMode: () => void
 }
 
+//create context that will handle the color theme of the page
 export const PageThemeContext = createContext<IPageContextValues>(undefined!)
-
 PageThemeContext.displayName = 'PageThemeContext'
 
+//custom context provider
 export function PageThemeContextProvider({children}: {children: ReactChildren}) {
-
+    //state that manages the color themes for the page
     const [pageTheme, setPageTheme] = useState('')
 
+    //function that toggles dark mode on and off
     function toggleDarkMode() {
         setPageTheme(formerTheme => formerTheme ? '' : 'darkMode')
     }
--- frontend/src/Components/PageLayout/index.tsx
+++ frontend/src/Components/PageLayout/index.tsx
@@ -1,22 +1,24 @@
 import Snackbar from "Components/Snackbar";
 import PageContainer from "./PageContainer";
 import ContentContainer from "./ContentContainer";
+import { useContext } from 'react'
 import { Outlet } from "react-router-dom";
 import Header from "Components/Header";
+import { PageThemeContext } from "Common/Contexts/PageThemeContext";
 
 function PageLayout() {
- 
+    const { pageTheme, toggleDarkMode } = useContext(PageThemeContext)
     return(
-        <>
+        <div className={pageTheme}>
             <Header/>
             <PageContainer>
                 <ContentContainer>
                     <Outlet/>
+                    <button onClick={() => toggleDarkMode()}>Test Dark Mode</button>
                 </ContentContainer>
-                
                 <Snackbar/>
             </PageContainer>
-        </>
+        </div>
     )
 }
 
