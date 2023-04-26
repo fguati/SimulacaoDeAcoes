4e6a426bbcc246d7e465ada07d759c6742e76afe
Fernando <fguatimosim@gmail.com>
2023-04-26 13:40:11 -0300

    feat: :sparkles: Created local storage for page theme preferences

--- frontend/src/Common/Contexts/PageThemeContext/index.tsx
+++ frontend/src/Common/Contexts/PageThemeContext/index.tsx
@@ -1,6 +1,9 @@
 import { ReactChildren } from "Common/Types";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { PageTheme } from "./types";
+
+//Key of the local storage object that 
+const storageThemePreferenceKey = 'themePreference'
 
 //Interface of values and methods provided by the page theme context
 interface IPageContextValues {
@@ -18,9 +21,26 @@ export function PageThemeContextProvider({children}: {children: ReactChildren}) 
     //state that manages the color themes for the page
     const [pageTheme, setPageTheme] = useState<PageTheme>('')
 
+    //checking if there is a theme preference stored locally and using it
+    useEffect(() => {
+        const themePreference = localStorage.getItem(storageThemePreferenceKey) as PageTheme | null
+
+        if(themePreference) {
+            setPageTheme(themePreference)
+        }
+    }, [])
+
     //function that toggles dark mode on and off
     function toggleDarkMode() {
-        setPageTheme(formerTheme => formerTheme ? '' : 'darkMode')
+        setPageTheme(formerTheme => {
+            if(formerTheme){
+                localStorage.removeItem(storageThemePreferenceKey)
+                return ''
+            }
+            
+            localStorage.setItem(storageThemePreferenceKey, 'darkMode')
+            return 'darkMode'
+        })
     }
 
     return (
--- frontend/src/Common/Contexts/PageThemeContext/test/PageThemeContext.test.tsx
+++ frontend/src/Common/Contexts/PageThemeContext/test/PageThemeContext.test.tsx
@@ -3,6 +3,39 @@ import {useContext} from 'react'
 import { PageThemeContext, PageThemeContextProvider } from '..'
 
 describe('Tests the context the determine the color theme of the page', () => {
+    const localStorageMock = (function () {
+        type Store = {[key: string]: string}
+        let store: Store = {};
+      
+        return {
+          getItem(key: string) {
+            return store[key];
+          },
+      
+          setItem(key: string, value: string) {
+            store[key] = value;
+          },
+      
+          clear() {
+            store = {};
+          },
+      
+          removeItem(key: string) {
+            delete store[key];
+          },
+      
+          getAll() {
+            return store;
+          },
+        };
+    })();
+      
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
+    afterEach(() => {
+        localStorage.clear()
+    })
+    
     const ExampleComponent = () => {
         const { pageTheme, toggleDarkMode } = useContext(PageThemeContext) 
         
@@ -43,6 +76,22 @@ describe('Tests the context the determine the color theme of the page', () => {
         fireEvent.click($toggleDarkModeButton)
         expect($themeState.textContent).toBe('darkMode')
 
+
+    })
+
+    test('toggleDarkMode function stores theme in the localStorage', () => {
+        render(
+            <PageThemeContextProvider>
+                <ExampleComponent/>
+            </PageThemeContextProvider>
+        )
+        
+        expect(localStorage.getItem('themePreference')).toBe(undefined)
+        
+        const $toggleDarkModeButton = screen.getByText('toggleDarkMode')
+        fireEvent.click($toggleDarkModeButton)
+        expect(localStorage.getItem('themePreference')).toBe('darkMode')
+        
     })
 
 })
