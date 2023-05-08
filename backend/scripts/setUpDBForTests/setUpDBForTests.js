51a4353b0a5f6ed08a8cb212076796e5083bdd34
Fernando <fguatimosim@gmail.com>
2023-05-08 18:21:07 -0300

    feat: :sparkles: Implemented the table negotiations on db
    Created negotiations table and already set up so both regular and test dbs include the table

--- backend/scripts/setUpDBForTests/setUpDBForTests.js
+++ backend/scripts/setUpDBForTests/setUpDBForTests.js
@@ -1,12 +1,14 @@
 const { dbFileDir } = require('#root/src/utils/globalVariables.js')
 const sqlite3 = require('sqlite3').verbose()
+const createNegotiationTableSQL = require('#root/src/db/Schemas/negotiationsSchema.js')
 const createUserTableSQL = require('#root/src/db/Schemas/userSchema.js')
 const createPositionsTableSQL = require('#root/src/db/Schemas/positionSchema.js')
-const { positionDbSql, userDbSql } = require('./testDbSql.js')
+const { positionDbSql, userDbSql, negotiationDbSql } = require('./testDbSql.js')
 
 function populateDBWithTestData(dbFilePath){
 	const deleteUserTable = `DROP TABLE IF EXISTS users;`
 	const deletePositionsTable = `DROP TABLE IF EXISTS stock_positions;`
+	const deleteNegotiationTable = `DROP TABLE IF EXISTS negotiations;`
 
     //conect to the database that will receive the test data
     const db = new sqlite3.Database(dbFilePath, (err) => {
@@ -22,8 +24,11 @@ function populateDBWithTestData(dbFilePath){
 		db.run(createUserTableSQL) 
 		db.run(deletePositionsTable)
 		db.run(createPositionsTableSQL) 
+		db.run(deleteNegotiationTable)
+		db.run(createNegotiationTableSQL) 
 		db.run(userDbSql)
 		db.run(positionDbSql)
+		db.run(negotiationDbSql)
 	})
 
 }
--- backend/src/db/Schemas/index.js
+++ backend/src/db/Schemas/index.js
@@ -1,6 +1,7 @@
 const USERS_SCHEMA = require('./userSchema.js')
 const POSITION_SCHEMA = require('./positionSchema.js')
+const NEGOTIATION_SCHEMA = require('./negotiationsSchema.js')
 
-SCHEMA_LIST = [USERS_SCHEMA, POSITION_SCHEMA]
+SCHEMA_LIST = [USERS_SCHEMA, POSITION_SCHEMA, NEGOTIATION_SCHEMA]
 
 module.exports = SCHEMA_LIST
