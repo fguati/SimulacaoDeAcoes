1028d3c930ffa1d4292f37d655b3707ac7d3e9a2
Fernando <fguatimosim@gmail.com>
2023-05-24 16:36:59 -0300

    refactor: :recycle: Refactored useFetchPortfolio custom hook

--- frontend/src/Pages/HomePage/utils/fetchPortfolio.ts
+++ frontend/src/Pages/HomePage/utils/fetchPortfolio.ts
@@ -8,30 +8,24 @@ import { handleErrorResponse } from "utils/BackendAPICommunication"
 import fetchFromServer from "utils/BackendAPICommunication/http/httpServerFetch"
 import transformErrorInResponse from "utils/BackendAPICommunication/responseHandlers/transformErrorInResponse"
 
-
+/**
+ * Custom hook that returns function that access the server and return the user portfolio
+ */
 function useFetchPortfolio() {
     const navigate = useNavigate()
 
     return useCallback(async () => {
         const url: BackendRoutes = '/user/portfolio'
+        
         try {
             const response = await fetchFromServer<IServerPositionRes[]>(url)
+            
             if(response.body && 'length' in response.body){
                 const listOfPositions = response.body!
-                const stockListFromServer: IStock[] = listOfPositions.map(position => {
-                    const stock: IStock = {
-                        id: position.stockTicker,
-                        companyName: position.stockTicker,//placeholder => pegar de finance API
-                        currentPrice: position.averagePrice,//placeholder => pegar de finance API
-                        qty: position.qty,
-                        ticker: position.stockTicker,
-                    }
-            
-                    stock.totalValue = stock.qty * stock.currentPrice
-                    return stock
-                })
+                
+                const stockList: IStock[] = listOfPositions.map(convertPositionToStock)
     
-                return stockListFromServer
+                return stockList
 
             }
 
@@ -48,6 +42,19 @@ function useFetchPortfolio() {
         }
         
     }, [navigate]) 
+}
+
+const convertPositionToStock = (position: IServerPositionRes): IStock => {
+    const stock: IStock = {
+        id: position.stockTicker,
+        companyName: position.stockTicker, //provisorio/placeholder. trocar quando fizer fetch para Finance API
+        currentPrice: position.averagePrice, //provisorio/placeholder. trocar quando fizer fetch para Finance API
+        qty: position.qty,
+        ticker: position.stockTicker,
+    }
+
+    stock.totalValue = stock.qty * stock.currentPrice
+    return stock
 }
 
 export default useFetchPortfolio
